35fc1b4cd268c3deefd90be9cbab6ab488ac7d13
vladimirSirin <shadowsong_sirin@hotmail.com>
2022-03-22 19:04:33 +0800

    Adding FPS AI guard with sight sense

---
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSAIGuard.h"
+#include "Perception/PawnSensingComponent.h"
+
+// Sets default values
+AFPSAIGuard::AFPSAIGuard()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
+
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnSeenPawn);
+
+}
+
+// Called when the game starts or when spawned
+void AFPSAIGuard::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called whens seeing a player pawn
+void AFPSAIGuard::OnSeenPawn(APawn* SeenPawn)
+{
+	if (SeenPawn == nullptr)
+	{
+		return;
+	}
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Yellow, false, 10.0f);
+}
+
+// Called every frame
+void AFPSAIGuard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSAIGuard::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
